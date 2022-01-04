3aed7b33028280e768360ffeda6a931733a2d098
samuel <Skboakye002@st.ug.edu.gh>
2022-01-04 16:46:51 +0000

    design and make about me section responsive

--- index.js
+++ index.js
@@ -21,3 +21,19 @@
  }
 
  
+window.onscroll = function() {myFunction()};
+ 
+let header = document.getElementById("header-el");
+
+ 
+let sticky = header.offsetTop;
+ 
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+ 
