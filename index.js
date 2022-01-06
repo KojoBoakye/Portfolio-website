578f13d795902c27ca5b7bc480853eb125567307
samuel <Skboakye002@st.ug.edu.gh>
2022-01-06 10:50:45 +0000

    finishing touches

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
  function toggle(){
   
     document.getElementById("icon").innerHTML="     <i  id='times-el' class=' fa fa-times' style='font-size:30px;'></i>"
-    document.getElementById("nav-el").style="position: fixed;top:20%; height:fit-content;width: 100%;  color: #ffffff; "
+    document.getElementById("nav-el").style="position: fixed;top:20%; height:fit-content;width: 100%;  color: #ffffff; border-radius:8px;"
    
 
     document.getElementById("times-el").addEventListener('click' , function(){
   
         document.getElementById("icon").innerHTML="     <i  id='bars-el' class=' fa fa-bars' onclick='toggle()' style='font-size:30px;'></i>"
-        document.getElementById("nav-el").style.cssText="position: fixed;top:-120%; height:  fit-content;width: 100vw;  color: #ffffff;"
-     
+        document.getElementById("nav-el").style.cssText="position: fixed;top:-190%; height:  fit-content;width: 100vw;  color: #ffffff;"
     })
  
 
